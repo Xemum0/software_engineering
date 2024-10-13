8a390877e090b7b05299a29eeb081e3e59532ff6
IHEBmk <145984657+IHEBmk@users.noreply.github.com>
2024-10-13 14:55:39 +0100

    Update Division.java

--- Calculator /Division.java
+++ Calculator /Division.java
@@ -1,16 +1,15 @@
 public class Division extends Calculator{
     @Override
     Double compute(Double A, Double B) {
-        if (B == 0) {
-            System.out.println("Error, No division by zero");
-            return 0.0;
-        }
-        try{
-            Double result = A / B;
-            return result;
-        }catch(Exception e){
+        if (B==0){
             System.out.println("Error,No division by zero");
             return 0.0;
         }
+        
+            Double result = A / B;
+            return result;
+   
+            
+  
     }
 }
