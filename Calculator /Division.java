19fb0ed2c9e0942ad3f57949c2b9e73920146051
youcef mhammdi bouzina <youcef.mhammdi.bouzina@ensia.edu.dz>
2024-10-13 14:53:46 +0100

    2

--- Calculator /Division.java
+++ Calculator /Division.java
@@ -1,6 +1,10 @@
 public class Division extends Calculator{
     @Override
     Double compute(Double A, Double B) {
+        if (B == 0) {
+            System.out.println("Error, No division by zero");
+            return 0.0;
+        }
         try{
             Double result = A / B;
             return result;
