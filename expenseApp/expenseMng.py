6afcf034b62f4071913efe3c012cfda5796b5ec8
AbderrahmeneTliba01010 <146717533+AbderrahmeneTliba01010@users.noreply.github.com>
2024-10-27 14:49:36 +0100

    Update expenseMng.py

--- expenseApp/expenseMng.py
+++ expenseApp/expenseMng.py
@@ -3,9 +3,9 @@ class ExpenseManager:
     def __init__(self, db):
         self.db = db
 
-    def add_expense(self, name, price):
+    def add_expense(self, name, price,date):
         if name and self.is_valid_price(price):
-            self.db.add_expense(name, float(price))
+            self.db.add_expense(name, float(price),date)
             return True
         return False
 
@@ -15,6 +15,8 @@ class ExpenseManager:
     def delete_expense(self, expense_id):
         self.db.delete_expense(expense_id)
 
+    def get_expenses_in_duration(self,start_date,end_date):
+        self.db.get_expenses_in_duration(start_date,end_date)
     def is_valid_price(self, price):
         try:
             float(price)
